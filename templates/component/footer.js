4d971468c87123611013e9a1ff360d5b890ae5e3
gj <1285803721@qq.com>
2016-08-24 17:53:19 +0800

    引入vue.js,成功模块化

---
+++ templates/component/footer.js
@@ -0,0 +1,13 @@
+Vue.component('my-component', {
+    template: `<div class="footer">
+            <div class="pull-right">
+                10GB of <strong>250GB</strong> Free.
+            </div>
+            <div>
+                <strong>Copyright</strong> Example Company &copy; 2014-2015
+            </div>
+        </div>`
+})
+new Vue({
+    el: document.body
+})
